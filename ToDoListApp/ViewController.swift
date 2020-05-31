08a125a1b26d85d91b8a5aad529ab6e7f6e57b27
Lydia-Tan <lydia.sydia@gmail.com>
2020-05-31 19:39:07 -0400

    Initial Commit

---
+++ ToDoListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoListApp
+//
+//  Created by Lydia Tan on 5/31/20.
+//  Copyright © 2020 Lydia Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
